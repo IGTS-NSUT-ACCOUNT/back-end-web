cc537d2d8668c1c2427f514d5d78b7997e1e0f10
parikshit <parikshit.p2002@gmail.com>
2023-05-24 02:19:33 +0530

    mm

--- routes/controllers/UserRouter.js
+++ routes/controllers/UserRouter.js
@@ -37,7 +37,7 @@ const {
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_CALLBACK_URL
+  process.env.GOOGLE_CLIENT_CALLBACK_URL
 );
 
 // Function to generate the authorization URL
