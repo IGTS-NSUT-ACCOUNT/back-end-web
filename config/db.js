9fb68c90b8bf050726031b4e50b58d64a7fc5018
pragun-bansal <pragun.bansal@yahoo.in>
2023-10-30 14:21:19 +0530

    route correction

--- config/db.js
+++ config/db.js
@@ -2,8 +2,10 @@ const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 const connectDB = async () => {
   try {
+    const pass = process.env.DB_PASS
+    const username = process.env.DB_HOST
     const conn = await mongoose.connect(
-      `mongodb+srv://igtsnsit:${process.env.DB_PASS}@cluster0.05efdbm.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${username}:${pass}@cluster0.05efdbm.mongodb.net/?retryWrites=true&w=majority`
     );
     console.log("Connection Successful");
   } catch (error) {
